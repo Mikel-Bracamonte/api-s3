3c9f42c4784a4387b4b941a2c1cbd0f9e0b59494
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-11-08 21:01:32 -0500

    tarea

---
+++ crear_bucket.py
@@ -0,0 +1,19 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    nombre_bucket = event['body']['bucket']
+    
+    # Proceso    
+    s3 = boto3.client('s3')
+    
+    response = s3.create_bucket(
+        ACL='public-read',
+        Bucket=nombre_bucket
+    )
+
+    return {
+        'statusCode': 200,
+        'bucket': nombre_bucket,
+        'location': response['Location']
+    }
