97ab5c2a0a9c3291f5615ebc6cc9f9d04b437493
Mikel_Bracamonte <mikel.bracamonte@utec.edu.pe>
2024-11-08 21:25:46 -0500

    crear_directorio

---
+++ crear_directorio.py
@@ -0,0 +1,21 @@
+import boto3
+
+def lambda_handler(event, context):
+    # Entrada (json)
+    nombre_bucket = event['body']['bucket']
+    directory = event['body']['directory']
+    
+    # Proceso    
+    s3 = boto3.client('s3')
+    
+    response = s3.put_object(
+        Bucket=nombre_bucket,
+        Body='',
+        Key=directory + '/'
+    )
+
+    return {
+        'statusCode': 200,
+        'bucket': nombre_bucket,
+        'location': response
+    }
